355bb00e01757634dfd6492c06d6eb2365c2a585
RyanR1019 <Rramirez1019@gmail.com>
2020-07-12 17:30:05 -0500

    Added SFX

--- Assets/Scripts/Ship_Movement.cs
+++ Assets/Scripts/Ship_Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Ship_Movement : MonoBehaviour
 {
-    public float Ship_Speed = 10f; 
+    public float Ship_Speed = 10f;
+    public float X_min;
+    public float X_max;
 
     // Update is called once per frame
     void Update()
@@ -17,7 +19,11 @@ public class Ship_Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Ship_Speed * Time.deltaTime);
-
+            
         }
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, X_min, X_max);
+        transform.position = position;
+        
     }
 }
